fb64031799b62cf6800220fddbab79850df51c64
Animesh Agarwal <animesh.agarwal.int20@ethara.ai>
2026-05-09 16:34:32 +0530

    fix: remove scratch test files that were breaking the production build

--- scratch/test-lib.ts
+++
@@ -1,8 +0,0 @@
-import { getPaintings } from "./src/lib/paintings.ts";
-
-async function run() {
-  const paintings = await getPaintings();
-  console.log("Paintings:", paintings.map(p => ({ title: p.title, shortDescription: p.shortDescription })));
-}
-
-run().catch(console.error);
